f97aee704d29eb726899ad0ab7344e5f38eb481e
Anssi Reponen <anssi.reponen@fmi.fi>
2018-11-22 09:29:12 +0200

    Anssin kehitysversio 22.11.2018

---
+++ macgyver/PostgreSQLConnection.h
@@ -0,0 +1,63 @@
+// ======================================================================
+/*!
+ * \brief Interface of class Fmi::Connection
+ */
+// ======================================================================
+
+#pragma once
+
+#include <boost/shared_ptr.hpp>
+#include <list>
+#include <pqxx/pqxx>
+#include <string>
+
+namespace Fmi
+{
+namespace Database
+{
+struct PostgreSQLConnectionOptions
+{
+  std::string host;
+  unsigned int port = 5432;
+  std::string database;
+  std::string username;
+  std::string password;
+  std::string encoding = "UTF8";
+  unsigned int connect_timeout = 0;
+  bool debug = false;
+};
+
+class PostgreSQLConnection
+{
+ public:
+  ~PostgreSQLConnection() { close(); }
+  PostgreSQLConnection(bool theDebug = false) : itsDebug(theDebug), itsCollate(false) {}
+  PostgreSQLConnection(const PostgreSQLConnectionOptions& theConnectionOptions);
+
+  bool open(const PostgreSQLConnectionOptions& theConnectionOptions);
+  bool reopen();
+
+  void close();
+  bool isConnected() const { return itsConnection->is_open(); }
+  void setClientEncoding(const std::string& theEncoding) const;
+  void setDebug(bool debug) { itsDebug = debug; }
+  pqxx::result executeNonTransaction(const std::string& theSQLStatement) const;
+  void startTransaction();
+  pqxx::result executeTransaction(const std::string& theSQLStatement) const;
+  void commitTransaction();
+
+  bool collateSupported() { return itsCollate; }
+  std::string quote(const std::string& theString) const;
+
+ private:
+  boost::shared_ptr<pqxx::connection> itsConnection;  // PostgreSQL connecton
+  boost::shared_ptr<pqxx::work> itsTransaction;       // PostgreSQL transaction
+  bool itsDebug;
+  bool itsCollate;
+  PostgreSQLConnectionOptions itsConnectionOptions;
+};  // class PostgreSQLConnection
+
+}  // namespace Database
+}  // namespace Fmi
+
+// ======================================================================
