32bb4055cf359823f97b13a6d21c31edd8556ced
Andris Pavenis <andris.pavenis@fmi.fi>
2019-07-15 12:59:22 +0300

    New version 19.7.29: Add class Fmi::TaskGroup.

---
+++ macgyver/TaskGroup.cpp
@@ -0,0 +1,110 @@
+#include "TaskGroup.h"
+
+Fmi::TaskGroup::TaskGroup(std::size_t max_parallel_tasks)
+  : max_parallel_tasks(max_parallel_tasks)
+  , counter(0)
+  , num_failures(0)
+{
+}
+
+Fmi::TaskGroup::~TaskGroup()
+{
+}
+
+void Fmi::TaskGroup::add(const std::string& name, std::function<void()> task)
+{
+  while (get_num_tasks() >= max_parallel_tasks) {
+    wait_some();
+  }
+
+  std::function<void()> task_impl = [this, task]()
+    {
+      try {
+	task();
+      } catch (...) {
+	notify();
+	throw;
+      }
+
+      notify();
+    };
+
+  std::unique_lock<std::mutex> lock(mutex);
+  task_list.push_back(std::make_pair(name, std::async(std::launch::async, task_impl).share()));
+}
+
+void Fmi::TaskGroup::wait()
+{
+  while (wait_some()) {
+  }
+}
+
+std::size_t Fmi::TaskGroup::get_num_tasks() const
+{
+  std::lock_guard<std::mutex> lock(mutex);
+  return task_list.size();
+}
+
+boost::signals2::connection
+Fmi::TaskGroup::on_task_ended(std::function<void(const std::string&)> callback)
+{
+  return signal_task_ended.connect(callback);
+}
+
+boost::signals2::connection
+Fmi::TaskGroup::on_task_error(std::function<void(const std::string&)> callback)
+{
+  return signal_task_failed.connect(callback);
+}
+
+bool Fmi::TaskGroup::wait_some()
+{
+  std::queue<std::pair<std::string, std::shared_future<void> > > finished_tasks;
+  std::unique_lock<std::mutex> lock(mutex);
+  if (task_list.empty()) {
+    return false;
+  } else {
+    extract_finished(finished_tasks);
+    if (finished_tasks.empty()) {
+      cond.wait(lock);
+      extract_finished(finished_tasks);
+    }
+  }
+
+  lock.unlock();
+
+  while (not finished_tasks.empty()) {
+    auto item = finished_tasks.front();
+    finished_tasks.pop();
+    try {
+      item.second.get();
+      signal_task_ended(item.first);
+    } catch (...) {
+      num_failures++;
+      signal_task_failed(item.first);
+    }
+  }
+
+  return true;
+}
+
+void
+Fmi::TaskGroup::extract_finished(std::queue<std::pair<std::string, std::shared_future<void> > >& finished_tasks)
+{
+  iterator curr, next;
+  for (curr = task_list.begin(); curr != task_list.end(); curr = next) {
+    next = curr;
+    ++next;
+    if (curr->second.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+      finished_tasks.push(*curr);
+      task_list.erase(curr);
+    }
+  }
+}
+
+void Fmi::TaskGroup::notify()
+{
+  std::unique_lock<std::mutex> lock(mutex);
+  cond.notify_all();
+};
+
---
+++ macgyver/TaskGroup.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <list>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <boost/signals2.hpp>
+
+namespace Fmi {
+
+class TaskGroup
+{
+ public:
+  TaskGroup(std::size_t max_parallel_tasks = 30);
+  virtual ~TaskGroup();
+
+  void add(const std::string& name, std::function<void()> task);
+  void wait();
+  std::size_t get_num_tasks() const;
+  std::size_t get_num_failures() const;
+
+  boost::signals2::connection on_task_ended(std::function<void(const std::string&)> callback);
+  boost::signals2::connection on_task_error(std::function<void(const std::string&)> callback);
+
+ private:
+  bool wait_some();
+  void remove_finished();
+  void extract_finished(std::queue<std::pair<std::string, std::shared_future<void> > >& finished_tasks);
+  void notify();
+
+ private:
+  typedef std::list<std::pair<std::string, std::shared_future<void> > >::iterator iterator;
+
+  mutable std::mutex mutex;
+  std::condition_variable cond;
+  std::size_t max_parallel_tasks;
+  std::size_t counter;
+  std::size_t num_failures;
+  std::list<std::pair<std::string, std::shared_future<void> > > task_list;
+  boost::signals2::signal<void(const std::string&)> signal_task_ended;
+  boost::signals2::signal<void(const std::string&)> signal_task_failed;
+};
+
+}
