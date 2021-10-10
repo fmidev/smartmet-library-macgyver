deac4614c8bc60efaeacaf515fed4ef61e218483
Andris Pavēnis <andris.pavenis@fmi.fi>
2021-10-10 15:08:55 +0300

    [tests] AsyncTaskTest: update
    - short delays (std::this::thread::sleep_for) are unreliable especially in virtual machines
      (eg. Oracle VirtuelBox). Adjust to avoid random failures as far a possible
    - add missing locking in notify test

--- test/AsyncTaskTest.cpp
+++ test/AsyncTaskTest.cpp
@@ -7,11 +7,15 @@
 
 #include "AsyncTask.h"
 #include "TypeName.h"
+#include <boost/chrono.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread.hpp>
 #include <regression/tframe.h>
 #include <atomic>
+#include <condition_variable>
 #include <iostream>
 #include <memory>
+#include <sstream>
 #include <string>
 #include <thread>
 
@@ -27,22 +31,19 @@ struct TestException
 void not_waiting_for_result()
 {
   std::atomic<int> test(0);
-  std::unique_ptr<Fmi::AsyncTask> task(new Fmi::AsyncTask("not waiting for result",
-                                                          [&]()
-                                                          {
-                                                            for (int i = 0; i < 100; i++)
-                                                            {
-                                                              Fmi::AsyncTask::interruption_point();
-                                                              std::this_thread::sleep_for(
-                                                                  std::chrono::milliseconds(5));
-                                                            }
-                                                            test = 1;
-                                                          }));
+
+  std::unique_ptr<Fmi::AsyncTask>
+    task(new Fmi::AsyncTask("not waiting for result",
+			    [&]()
+			    {
+			      boost::this_thread::sleep_for(boost::chrono::seconds(5));
+			      test = 1;
+			    }));
   std::this_thread::sleep_for(std::chrono::milliseconds(10));
   ptime t1 = microsec_clock::universal_time();
   task.reset();
   ptime t2 = microsec_clock::universal_time();
-  if ((t2 - t1).total_milliseconds() > 50)
+  if ((t2 - t1).total_milliseconds() > 200)
   {
     // std::cout << "\n" << (t2 - t1) << std::endl;
     TEST_FAILED("Task not stopped as expected when wait for completion missing");
@@ -81,6 +82,7 @@ void single_task()
 
 void task_throws_not_std_exception()
 {
+  bool got_expected_exception = false;
   std::string exception_name;
   std::string task_name;
   Fmi::AsyncTask task("foobar", []() { throw TestException(); });
@@ -91,6 +93,7 @@ void task_throws_not_std_exception()
   }
   catch (const TestException&)
   {
+    got_expected_exception = true;
   }
   catch (const tframe::failed&)
   {
@@ -99,6 +102,10 @@ void task_throws_not_std_exception()
   catch (...)
   {
     TEST_FAILED("Unexcpected exception of type '" + Fmi::current_exception_type() + "' thrown");
+  }
+  if (!got_expected_exception)
+  {
+    TEST_FAILED("Did not get expected exception TestException from AsyncTask");
   }
   if (task.get_status() != Fmi::AsyncTask::failed)
   {
@@ -109,44 +116,110 @@ void task_throws_not_std_exception()
 
 void test_cancel_task()
 {
-  Fmi::AsyncTask task("foobar",
-                      []()
-                      {
-                        for (int i = 0; i < 100; i++)
-                        {
-                          std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                          Fmi::AsyncTask::interruption_point();
-                        }
-                      });
+  ptime tm1, tm2, tm3;
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(5));
-  ptime t1 = microsec_clock::universal_time();
-  task.cancel();
-  task.wait();
-  ptime t2 = microsec_clock::universal_time();
-  if (task.get_status() != Fmi::AsyncTask::interrupted)
-  {
-    TEST_FAILED("Not expected test status");
+  std::ostringstream debug_info;
+
+  try {
+    ptime t1 = microsec_clock::universal_time();
+    Fmi::AsyncTask
+      task("foobar",
+	   [&]()
+	   {
+	     try {
+	       tm1 = microsec_clock::universal_time();
+	       boost::this_thread::sleep_for(boost::chrono::milliseconds(5000));
+	       tm2 = microsec_clock::universal_time();
+	     } catch (const boost::thread_interrupted&) {
+	       tm3 = microsec_clock::universal_time();
+	       throw;
+	     }
+	   });
+
+    ptime t2 = microsec_clock::universal_time();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    ptime t3 = microsec_clock::universal_time();
+    task.cancel();
+    task.wait();
+    ptime t4 = microsec_clock::universal_time();
+
+    debug_info << "started            : " << t1 << '\n'
+	       << "task created       : " << t2 << '\n'
+	       << "task started       : " << tm1 << '\n'
+	       << "task ended         : " << tm2 << " (should not happen)\n"
+	       << "got interrupted    : " << tm3 << '\n'
+	       << "about to interrupt : " << t3 << '\n'
+	       << "done               : " << t4 << '\n'
+	       << "dt                 : " << (t4-t3) << std::endl;
+
+    if (task.get_status() != Fmi::AsyncTask::interrupted)
+      {
+	TEST_FAILED("Not expected test status");
+      }
+
+    const auto dt = (t2 - t1).total_milliseconds();
+    if (dt > 100)
+      {
+	TEST_FAILED("Canceling task took too long time (" + std::to_string(dt) + "milliseconds)");
+      }
+
   }
-  if ((t2 - t1).total_milliseconds() > 50)
+  catch (const tframe::failed&)
   {
-    TEST_FAILED("Canceling task took too long time");
+    std::cout << std::endl << debug_info.str();
+    throw;
   }
   TEST_PASSED();
 }
 
 void test_notify()
 {
+  ptime tm1, tm2, tm3;
   std::mutex m;
   std::condition_variable cond;
+  bool ended = false;
+  const ptime t1 = microsec_clock::universal_time();
   Fmi::AsyncTask task(
       "foobar",
-      []() { std::this_thread::sleep_for(std::chrono::milliseconds(50)); },
-      [&cond]() { cond.notify_all(); });
+      [&tm1, &tm2]()
+      {
+	tm1 = microsec_clock::universal_time();
+	std::this_thread::sleep_for(std::chrono::milliseconds(5));
+	tm2 = microsec_clock::universal_time();
+      },
+      [&cond, &m, &ended, &tm3]()
+      {
+	std::unique_lock<std::mutex> lock(m);
+	ended = true;
+	cond.notify_all();
+	tm3 = microsec_clock::universal_time();
+      });
+  const ptime t2 = microsec_clock::universal_time();
 
   std::unique_lock<std::mutex> lock(m);
-  if (!cond.wait_for(lock, std::chrono::milliseconds(100), [&task]() { return task.ended(); }))
+  const auto got_notification =
+    cond.wait_for(lock,
+		   std::chrono::milliseconds(5000),
+		  [&task]()
+		  {
+		    return task.ended();
+		  });
+
+  const ptime t3 = microsec_clock::universal_time();
+
+  std::ostringstream debug_info;
+  debug_info << "--------------- Test notify ----------------\n";
+  debug_info << "started            : " << t1 << '\n';
+  debug_info << "task created       : " << t2-t1 << '\n';
+  debug_info << "timed wait done    : " << t3-t1 << " (" << t3-t2 << '\n';
+  debug_info << "task started       : " << tm1-t1 << '\n';
+  debug_info << "task ended         : " << tm2-t1 << '\n';
+  debug_info << "notification sent  : " << t3-t1 << '\n';
+
+  if (!got_notification)
   {
+    std::cout << std::endl << debug_info.str() << std::endl;
     TEST_FAILED("Did not get notification about task end");
     task.cancel();
   }
