95cb831a17070c7851d5f0cd5425fae26ea329b4
Mika Heiskanen <mika.heiskanen@fmi.fi>
2022-01-19 06:31:11 +0200

    clang-format

--- macgyver/DistanceParser.h
+++ macgyver/DistanceParser.h
@@ -1,9 +1,9 @@
 // ======================================================================
 /*!
- * \brief Parse distances. 
+ * \brief Parse distances.
  *
  * Known formats: kilometer,meter,centimeter,millimeter,mile,nautical_mile,yard,foot,inch
-*  Known inits: km,m,cm,mm,mi,nmi,yd,ft,in
+ *  Known inits: km,m,cm,mm,mi,nmi,yd,ft,in
  *
  */
 // ======================================================================
@@ -16,18 +16,17 @@ namespace Fmi
 {
 namespace DistanceParser
 {
-  double parse(const std::string& str,
-			   const std::string& format);
+double parse(const std::string& str, const std::string& format);
 
-  double parse_kilometer(const std::string& str);
-  double parse_meter(const std::string& str);
-  double parse_centimeter(const std::string& str);
-  double parse_millimeter(const std::string& str);
-  double parse_mile(const std::string& str);
-  double parse_nautical_mile(const std::string& str);
-  double parse_foot(const std::string& str);
-  double parse_yard(const std::string& str);
-  double parse_inch(const std::string& str);
+double parse_kilometer(const std::string& str);
+double parse_meter(const std::string& str);
+double parse_centimeter(const std::string& str);
+double parse_millimeter(const std::string& str);
+double parse_mile(const std::string& str);
+double parse_nautical_mile(const std::string& str);
+double parse_foot(const std::string& str);
+double parse_yard(const std::string& str);
+double parse_inch(const std::string& str);
 
 }  // namespace DistanceParser
 }  // namespace Fmi
