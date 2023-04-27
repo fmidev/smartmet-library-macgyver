1e4c98b0bbd4bd9801cf342c27eed51ec4ca8850
Andris Pavēnis <andris.pavenis@fmi.fi>
2023-04-27 06:36:12 +0000

    [tests] PostgreSQLConnection: update compile/only test

--- test/PostgreSQLConnectionCompile.cpp
+++ test/PostgreSQLConnectionCompile.cpp
@@ -3,6 +3,12 @@
 void test1(Fmi::Database::PostgreSQLConnection& conn)
 {
     conn.prepare("test", "SELECT * FROM foo WHERE x=$1");
-    Fmi::Database::PostgreSQLConnection::Transaction work(conn);
-    work.executePrepared("test", 42);
+    Fmi::Database::PostgreSQLConnection::PreparedSQL sql(
+        conn, "test",  "SELECT * FROM foo WHERE bar=$1 and baz=$2");
+    sql.exec(42, "something");
+}
+
+void test2(Fmi::Database::PostgreSQLConnection& conn)
+{
+    conn.exec_params("SELECT * FROM foo WHERE x=$1", 42);
 }
