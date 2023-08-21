b3d4e63df669928216cb27e5d09e557e5323284b
Andris Pavēnis <andris.pavenis@fmi.fi>
2023-08-21 06:52:00 +0300

    TimeParserDefinitions: include boost/phoenix.hpp instead of deprecated boost/spirit/include/phoenix.hpp

--- macgyver/TimeParserDefinitions.h
+++ macgyver/TimeParserDefinitions.h
@@ -8,7 +8,7 @@
 
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/optional.hpp>
-#include <boost/spirit/include/phoenix.hpp>
+#include <boost/phoenix.hpp>
 #include <boost/spirit/include/qi.hpp>
 
 #include <string>
