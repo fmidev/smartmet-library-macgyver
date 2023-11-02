39a0107b8d009430c08ee39f17c7579f31e81dfa
Andris Pavēnis <andris.pavenis@fmi.fi>
2023-11-02 13:58:55 +0200

    DateTime.h: extract common part into a separate file

--- macgyver/DateTime.h
+++ macgyver/DateTime.h
@@ -1,38 +1,10 @@
 #pragma once
 
-#include <chrono>
+#include "DateTimeCommon.h"
 #include <ostream>
-#include <boost/date_time/posix_time/posix_time.hpp>
-
-#if __cplusplus < 202002L
-#include <date/date.h>
-#endif
 
 namespace Fmi
 {
-#if __cplusplus >= 202002L
-  namespace date_ns = std::chrono;
-#else
-  namespace date_ns = date;
-#endif
-
-  using Date = boost::gregorian::date;
-  using TimeDuration = boost::posix_time::time_duration;
-
-  namespace detail
-  {
-
-    using period_t = std::micro;
-    using duration_t = std::chrono::duration<int64_t, std::micro>;
-    using time_point_t = std::chrono::time_point<date_ns::local_t, duration_t>;
-    using seconds_t = std::chrono::duration<int64_t, std::ratio<1, 1> >;
-    using days_t = std::chrono::duration<int64_t, std::ratio<86400, 1> >;
-
-    constexpr int periods_per_mks = std::micro::den * period_t::num / period_t::den;
-
-    static_assert (periods_per_mks * period_t::den / period_t::num == std::micro::den,
-          "INTERNAL ERROR");
-  }
 
   class DateTime : public detail::time_point_t
   {
