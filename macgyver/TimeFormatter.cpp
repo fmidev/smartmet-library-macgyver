0f4c67a0161817fe8e9b422c7c79ad5f2bf27208
Andris Pavēnis <andris.pavenis@fmi.fi>
2024-02-06 11:23:43 +0200

    TimeFormatter.cpp: update

--- macgyver/TimeFormatter.cpp
+++ macgyver/TimeFormatter.cpp
@@ -9,75 +9,10 @@
 #include "StringConversion.h"
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/predicate.hpp>
-#include <boost/date_time/gregorian/gregorian.hpp>
 #include <fmt/format.h>
 #include <fmt/printf.h>
 #include <array>
 #include <stdexcept>
-
-// ----------------------------------------------------------------------
-/*!
- * A local help subroutine to convert a UTC tm to UTC time_t
- * This code is copied from newbase NFmiStaticTime::my_timegm -method.
- *
- * The original C code is by C.A. Lademann and Richard Kettlewell.
- *
- * \param t The UTC time as a tm struct
- * \return The UTC time as a time_t
- * \bug This has not been verified to work in SGI/Windows
- */
-// ----------------------------------------------------------------------
-
-static time_t my_timegm(struct tm* t)
-{
-  try
-  {
-#if 0
-    // THIS IS NOT THREAD SAFE IF LOCALTIME_R IS CALLED SIMULTANEOUSLY!!!
-    return ::timegm(t);  // timegm is a GNU extension
-
-#else  // Windows
-    const int MINUTE = 60;
-    const int HOUR = 60 * MINUTE;
-    const int DAY = 24 * HOUR;
-    const int YEAR = 365 * DAY;
-
-    const std::array<int, 12> mon{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    if (t->tm_year < 70)
-      return (static_cast<time_t>(-1));
-
-    int n = t->tm_year + 1900 - 1;
-    time_t epoch = (t->tm_year - 70) * YEAR +
-                   ((n / 4 - n / 100 + n / 400) - (1969 / 4 - 1969 / 100 + 1969 / 400)) * DAY;
-
-    int y = t->tm_year + 1900;
-    int m = 0;
-    for (int i = 0; i < t->tm_mon; i++)
-    {
-      epoch += mon[m] * DAY;
-      if (m == 1 && y % 4 == 0 && (y % 100 != 0 || y % 400 == 0))
-        epoch += DAY;
-      if (++m > 11)
-      {
-        m = 0;
-        y++;
-      }
-    }
-
-    epoch += (t->tm_mday - 1) * DAY;
-    epoch += t->tm_hour * HOUR;
-    epoch += t->tm_min * MINUTE;
-    epoch += t->tm_sec;
-
-    return epoch;
-#endif
-  }
-  catch (...)
-  {
-    throw Fmi::Exception::Trace(BCP, "Operation failed!");
-  }
-}
 
 namespace Fmi
 {
@@ -300,8 +235,7 @@ std::string EpochFormatter::format(const DateTime& t) const
 {
   try
   {
-    tm tmp = boost::posix_time::to_tm(t);
-    time_t epo = ::my_timegm(&tmp);
+    time_t epo = (t - Fmi::Date::epoch).total_seconds();
     return Fmi::to_string(epo);
   }
   catch (...)
@@ -320,8 +254,8 @@ std::string EpochFormatter::format(const Fmi::LocalDateTime& t) const
 {
   try
   {
-    tm tmp = boost::posix_time::to_tm(t.utc_time());
-    time_t epo = ::my_timegm(&tmp);
+    const auto tmp = t.utc_time();
+    time_t epo = (tmp - Fmi::Date::epoch).total_seconds();
     return Fmi::to_string(epo);
   }
   catch (...)
