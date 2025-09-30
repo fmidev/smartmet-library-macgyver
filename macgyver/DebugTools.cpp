5c93e7efd8b88ff6f7966df55d4b256a51fca28b
Mika Heiskanen <mika.heiskanen@fmi.fi>
2025-09-30 19:20:07 +0000

    Use Fmi::stoi for consistency

--- macgyver/DebugTools.cpp
+++ macgyver/DebugTools.cpp
@@ -1,9 +1,9 @@
 #include "DebugTools.h"
+#include "StringConversion.h"
 #include <cstdio>
 #include <ctime>
 #include <iostream>
 #include <sstream>
-
 #include <fstream>
 #include <vector>
 #include <boost/algorithm/string/classification.hpp>
@@ -71,7 +71,7 @@ int Fmi::tracerPid()
         std::vector<std::string> parts;
         split(parts, line, is_any_of(" \t\r\n"), token_compress_on);
         if (parts.size() == 2 && parts[0] == "TracerPid:") {
-          return std::strtol(parts[1].c_str());
+          return Fmi::stoi(parts[1]);
         }
     }
 #endif
